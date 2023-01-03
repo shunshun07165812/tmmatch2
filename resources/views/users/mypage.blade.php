ae5c4e01a9a81d69a8e51202f0cf84928f36dd86
shunshun07165812 <junjiebeicun@gmail.com>
2023-01-03 20:52:06 +0900

    変更

--- resources/views/users/mypage.blade.php
+++ resources/views/users/mypage.blade.php
@@ -43,11 +43,9 @@
         <form method="POST" action="{{ route('logout') }}">
             @csrf
 
-            <x-dropdown-link :href="route('logout')"
-                    onclick="event.preventDefault();
-                                this.closest('form').submit();">
-                {{ __('Log Out') }}
-            </x-dropdown-link>
+            
+                <button>logout</button>
+            
         </form>
         
         
